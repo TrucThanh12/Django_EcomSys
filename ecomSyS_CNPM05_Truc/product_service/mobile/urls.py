066936abf72c11e59f5b7afb1f3451bcdacf67bf
ctv_trucpt <trucdien1812@gmail.com>
2024-04-24 20:49:20 +0700

    update

--- ecomSyS_CNPM05_Truc/product_service/mobile/urls.py
+++ ecomSyS_CNPM05_Truc/product_service/mobile/urls.py
@@ -1,3 +1,24 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
 urlpatterns = [
+    # path('category/add/', CreateCategoryView.as_view()),
+    # path('category/all/',CategoryListView.as_view()),
+    # path('category/delete/<str:category_id>/',DeleteCategoryView.as_view()),
+    # path('author/add/', CreateAuthorView.as_view()),
+    # path('author/delete/<str:author_id>/',DeleteAuthorView.as_view()),
+    # path('publisher/add/', CreatePublisherView.as_view()),
+    # path('publisher/delete/<str:publisher_id>/',DeletePublisherView.as_view()),
+    # path('add/', AddBookView.as_view()),
+    # path('list/',BookListView.as_view()),
+    # path('detail/<str:book_id>/',BookDetailView.as_view()),
+    # path('list/category/<str:category_id>/', BookListOfCategoryView.as_view()),
+    # path('search/<str:key>/',SearchBookListView.as_view()),
+    # path('edit/<str:book_id>',UpdateBookView.as_view()),
+    # path('delete/<str:book_id>/',DeleteBookView.as_view())
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
